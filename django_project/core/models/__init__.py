d1be3221079affdfb84724cd30e4893c5aa6830d
Morgan Tenney <morgan.tenney@swiftsolar.com>
2023-12-17 10:31:55 -0800

    move collection to its own models folder

--- django_project/core/models/__init__.py
+++ django_project/core/models/__init__.py
@@ -1,1 +1,4 @@
 from core.models.metadata.models import *
+from core.models.collection.models import *
+from core.models.rating.models import *
+from core.models.item.models import *
--- django_project/core/models/item/models.py
+++ django_project/core/models/item/models.py
@@ -31,17 +31,3 @@ class ItemRating(Metadata):
 
 #     def __str__(self):
 #         return self.name
-
-
-class CollectionType(Metadata):
-    name = models.CharField(max_length=128)  # 'farmed', 'wild-caught', etc.
-
-    def __str__(self):
-        return self.name
-
-
-class CollectionMethod(Metadata):
-    name = models.CharField(max_length=128)  # 'gillnets', 'longline', 'recirculating tanks', etc.
-
-    def __str__(self):
-        return self.name
