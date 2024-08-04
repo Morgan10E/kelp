6d8891d3ae0b15053c5ff124f77949b19eee27db
Morgan Tenney <morgan.tenney@gmail.com>
2024-08-03 19:06:27 -0700

    Simplify models; remove collection type/collection method structured fields and condense into single charfield; remove alternative name (leave as TODO)

--- django_project/core/models/item/models.py
+++ django_project/core/models/item/models.py
@@ -3,31 +3,11 @@ from core.models.metadata.models import Metadata
 
 
 class Item(Metadata):
-    primary_name = models.CharField(max_length=255, unique=True)
+    primary_name = models.CharField(max_length=255, unique=True)  # TODO: support alternative names, example: "red salmon", "sockeye salmon"
     description = models.TextField(blank=True, null=True)
 
 
 class ItemRating(Metadata):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     rating = models.ForeignKey('Rating', on_delete=models.CASCADE)
-    # region = models.ForeignKey('Region', on_delete=models.CASCADE)
-    collection_type = models.ForeignKey('CollectionType', on_delete=models.CASCADE)  # if blank, assume rating applies to all of 'item'
-    collection_method = models.ForeignKey('CollectionMethod', blank=True, null=True, on_delete=models.CASCADE)  # if blank, assume rating applies to all of 'collection_type'; if 'collection_type' is blank, this must also be blank
-
-
-# class AlternativeName(Metadata):
-#     name = models.CharField(max_length=255)
-#     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return self.name
-
-#     class Meta:
-#         verbose_name_plural = 'Alternative Names'
-
-
-# class Region(Metadata):
-#     name = models.CharField(max_length=255)  # 'Northwest Pacific', 'New Zealand', etc.
-
-#     def __str__(self):
-#         return self.name
+    collection_type = models.CharField(max_length=255)  # example: "intensive farming", "wild, line-caught"; if blank, assume rating applies to all of 'item'
--- django_project/core/models/rating/models.py
+++ django_project/core/models/rating/models.py
@@ -4,6 +4,6 @@ from core.models.metadata.models import Metadata
 
 class Rating(Metadata):
     name = models.CharField(max_length=128)
-    color = models.CharField(max_length=7) # hex color
+    color = models.CharField(max_length=6) # hex color
     description = models.TextField()
     relative_score = models.IntegerField() # 'goodness' relative to other ratings; lower is better
