e59ba82ebc3a6b38bd04a7d8ed72216b8b3973a7
Morgan Tenney <morgan.tenney@gmail.com>
2024-08-03 19:32:53 -0700

    Combine rating name and color; add services for splitting on delimiter

--- django_project/core/models/rating/models.py
+++ django_project/core/models/rating/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from core.models.metadata.models import Metadata
 
 
+NAME_COLOR_DELIMITER = ';'
+
+
 class Rating(Metadata):
-    name = models.CharField(max_length=128)
-    color = models.CharField(max_length=6) # hex color
+    name_and_color = models.CharField(max_length=128, unique=True)
     description = models.TextField()
     relative_score = models.IntegerField() # 'goodness' relative to other ratings; lower is better
